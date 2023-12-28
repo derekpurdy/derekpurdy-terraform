43f2b2e3c5a200f6f341a260c5f6195508afc946
Derek Purdy <10130789+derekpurdy@users.noreply.github.com>
2023-12-28 10:41:50 -0500

    test

---
+++ development/main.tf
@@ -0,0 +1,36 @@
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "ssh-allowed" {
+  name        = "ssh-allowed"
+  description = "Allow SSH"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ssh-allowed"
+  }
+}
